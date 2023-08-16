3f0004b03f38430dd6e7b38eb3e923e478a22c69
vkola <vkolachi@gmail.com>
2023-08-16 17:38:03 +0530

    add oops files

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+//       Animal a=new Animal();
+//       a.setEars(4);
+//        System.out.println(a.getEars());
+//        System.out.printf("Hello and welcome!");
+        Animal z=new Animal();
+        z.raj();
+        System.out.println(z.ears);
+
+
+        }
+    }
